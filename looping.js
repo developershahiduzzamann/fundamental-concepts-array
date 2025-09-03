c2ec723c3f78eabfe5dded450ff8d8d29ac57b38
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-03 06:13:26 +0600

    addad the looping js file

--- looping.js
+++ looping.js
@@ -1,4 +1,25 @@
 const friends = ['sabbir', 'sakib', 'habib', 'hasib'];
-for(const friend of friends){
-    console.log(friend);
+// for(const friend of friends){
+//     console.log(friend);
+// }
+// for(let i = 1; i <= friends.length; i++){
+//     console.log(i);
+//     console.log(friends[i]);
+// }
+
+let numbers = [11, 12, 13, 14, 15];
+// for(i = 0; i <= numbers.length; i++){
+//     // console.log(numbers[i]);
+// }
+
+let i = 0;
+while(i < friends.length){
+    console.log(friends[i]);
+    i++;
+}
+
+let n = 0;
+while(n < numbers.length){
+    console.log(numbers[n]);
+    n++;
 }
