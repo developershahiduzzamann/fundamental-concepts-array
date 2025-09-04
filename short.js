997674155e4f12a9e9b474f3a8da475e918063da
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-04 06:56:26 +0600

    add the short file

--- short.js
+++ short.js
@@ -1,5 +1,3 @@
-const number = [11, 12, 13, 14, 15, 16, 17];
-for(let i = number.length - 1; i >=0; i--){
-    const num = number[i];
-    console.log(num)
-}
+const persons = ['sabbir', 'sakib', 'habib', 'hasib', 'akib'];
+const sortedPerson = persons.sort();
+console.log(sortedPerson);
