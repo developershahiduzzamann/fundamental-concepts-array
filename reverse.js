bce230248523f18f2dc45d78c152d8d13d097f04
developrershahiduzzamann <sabbir.mirpur58@gmail.com>
2025-09-04 06:46:47 +0600

    addad the new line

--- reverse.js
+++ reverse.js
@@ -1,3 +1,18 @@
 const subject = ['bangla', 'english', 'math'];
-const rev = subject.reverse();
-console.log(rev);
+// const rev = subject.reverse();
+// console.log(rev);
+
+const reversedSubjects = [];
+
+for (let i = subject.length - 1; i >= 0; i--) {
+  reversedSubjects.push(subject[i]);
+}
+
+console.log(reversedSubjects)
+
+
+const number = [11, 12, 13, 14, 15, 16, 17];
+for(let i = number.length - 1; i >=0; i--){
+    const num = number[i];
+    console.log(num)
+}
---
+++ short.js
@@ -0,0 +1,5 @@
+const number = [11, 12, 13, 14, 15, 16, 17];
+for(let i = number.length - 1; i >=0; i--){
+    const num = number[i];
+    console.log(num)
+}
